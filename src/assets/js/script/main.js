7260c1cd034192683481d920bd11f59a1edf31f5
Leonardo Matos <leomp120894@gmail.com>
2018-02-06 12:37:36 -0200

    initial template

---
+++ src/assets/js/script/main.js
@@ -0,0 +1,35 @@
+'use strict';
+
+require('./config');
+require('./util.js');
+
+/*
+|--------------------------------------------------------------------------
+| Application Is Ready
+|--------------------------------------------------------------------------
+|
+| When all the dependencies of the page are loaded and executed,
+| the application automatically call this function. You can consider it as
+| a replacer for jQuery ready function - "$( document ).ready()".
+|
+*/
+
+app.ready(function() {
+
+  var resources = {
+    'applications': {
+      'label': {
+        'en_us': 'Apps'
+      },
+      'icon': 'puzzle-piece'
+    },
+    'authentications': {
+      'label': {
+        'en_us': 'Users',
+        'pt_br': 'Usuários'
+      },
+      'icon': 'users'
+    }
+  }
+
+});
