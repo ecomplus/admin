72440f8383f742321d850f165868e3d2cf5a05ff
Leonardo Matos <leomp120894@gmail.com>
2018-02-15 17:39:47 -0200

    update script

--- src/assets/js/routes/resources.js
+++ src/assets/js/routes/resources.js
@@ -4,11 +4,25 @@
 
 'use strict'
 
-var resourceSlug = window.routeParams[0]
-var resource = window.apiResources[resourceSlug]
-var lang = window.lang
+var Route = function () {
+  var slug = window.routeParams[0]
+  var resource = window.apiResources[slug]
 
-$('#resource-name').text(resource.label[lang])
+  var lang = window.lang
 
-// display content
-$('#route-content > *').fadeIn()
+  var action = window.routeParams[1]
+  if (!action) {
+    action = 'list'
+  }
+
+  var dictionary = {
+    list: {
+    }
+  }
+
+  $('#resource-name').html('<strong>' + resource.label[lang] + '</strong>: Lista')
+
+  // display content
+  $('#route-content > *').fadeIn()
+}
+Route()
