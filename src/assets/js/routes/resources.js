cea216886c385bb770c6d5dbd5bf85c3006f3e8d
Leonardo Matos <leomp120894@gmail.com>
2018-02-15 18:56:04 -0200

    render breadcrumbs

--- src/assets/js/routes/resources.js
+++ src/assets/js/routes/resources.js
@@ -14,6 +14,19 @@ var Route = function () {
   var action = window.routeParams[1]
   if (!action) {
     action = 'list'
+  } else {
+    switch (action) {
+      case 'create':
+      case 'edit':
+      case 'delete':
+        // continue
+        break
+
+      default:
+        // invalid action
+        window.e404()
+        return
+    }
   }
 
   var dictionary = {
@@ -21,10 +34,38 @@ var Route = function () {
     'list': i18n({
       'en_us': 'List',
       'pt_br': 'Listar'
+    }),
+    'create': i18n({
+      'en_us': 'Create',
+      'pt_br': 'Criar'
+    }),
+    'edit': i18n({
+      'en_us': 'Edit',
+      'pt_br': 'Editar'
+    }),
+    'delete': i18n({
+      'en_us': 'Delete',
+      'pt_br': 'Deletar'
     })
   }
 
-  $('#resource-name').html('<strong>' + resource.label[lang] + '</strong> · ' + dictionary[action])
+  // initial rendering
+  var html
+
+  // render H1
+  html = '<strong>' + resource.label[lang] + '</strong> · ' + dictionary[action]
+  $('#resource-name').html(html)
+
+  // render breadcrumb links
+  html = '<li class="breadcrumb-item">' +
+               '<a href="/#/resources/' + slug + '">' +
+                 '<i class="fa fa-' + resource.icon + '"></i> ' + resource.label[lang] +
+               '</a>' +
+             '</li>' +
+             '<li class="breadcrumb-item active">' +
+               dictionary[action] +
+             '</li>'
+  $('#breadcrumbs').append(html)
 
   // display content
   $('#route-content > *').fadeIn()
