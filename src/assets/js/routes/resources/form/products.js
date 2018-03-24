08c821cd338f69d5289ad0cf81ccaa180d9908d8
Leonardo Matos <leomp120894@gmail.com>
2018-03-24 19:30:51 -0300

    starting with product creation form

---
+++ src/assets/js/routes/resources/form/products.js
@@ -0,0 +1,14 @@
+/*!
+ * Copyright 2018 E-Com Club
+ */
+
+(function () {
+  'use strict'
+
+  // current tab ID
+  var tabId = window.tabId
+  var elContainer = $('#' + tabId + '-tab-normal')
+  // prefix tab ID on content elements IDs
+  window.renderContentIds(elContainer)
+  window.setSaveAction()
+}())
