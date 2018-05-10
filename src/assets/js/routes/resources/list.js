d09cfb61265b495e6041c5a05f066f4a0e2ac4dd
Leonardo Matos <leomp120894@gmail.com>
2018-05-10 15:30:16 -0300

    jsgrid template and styles

--- src/assets/js/routes/resources/list.js
+++ src/assets/js/routes/resources/list.js
@@ -25,8 +25,19 @@
     autoload: true,
     paging: true,
     pageLoading: true,
-    pageSize: 15,
-    pageIndex: 2,
+    pageButtonCount: 7,
+    pagerFormat: '{first} {prev} {pages} {next} {last} {pageIndex} / {pageCount}',
+    pagePrevText: '<',
+    pageNextText: '>',
+    pageFirstText: '<<',
+    pageLastText: '>>',
+    filtering: true,
+    sorting: true,
+    confirmDeleting: false,
+    rowClick: function (args) {
+      var id = args.item._id
+      console.log(id)
+    },
 
     controller: {
       loadData: function (filter) {
@@ -37,11 +48,57 @@
       }
     },
 
-    fields: [
-      { name: 'Name', type: 'text', width: 150 },
-      { name: 'Age', type: 'number', width: 50 },
-      { name: 'Address', type: 'text', width: 200 },
-      { name: 'Married', type: 'checkbox', title: 'Is Married' }
-    ]
+    fields: [{
+      /*
+      headerTemplate: function () {
+        var el = $('<button>').attr('type', 'button').addClass('btn btn-xs btn-danger')
+        el.html('Deletar')
+        el.on('click', function () {
+          deleteSelectedItems()
+        })
+        return el
+      },
+      */
+      itemTemplate: function (_, item) {
+        var el = $('<input>').attr('type', 'checkbox')
+        el.prop('checked', $.inArray(item, selectedItems) > -1)
+        el.on('change', function () {
+          $(this).is(':checked') ? selectItem(item) : unselectItem(item)
+        })
+        return el
+      },
+      align: 'center',
+      css: 'data-list-delete',
+      filtering: false,
+      sorting: false
+    }, {
+      name: '_id',
+      title: 'ID',
+      type: 'text'
+    }, {
+      name: 'sku',
+      title: 'SKU',
+      type: 'text'
+    }]
   })
+
+  var selectedItems = []
+
+  var selectItem = function (item) {
+    selectedItems.push(item)
+  }
+
+  var unselectItem = function (item) {
+    selectedItems = $.grep(selectedItems, function (i) {
+      return i !== item
+    })
+  }
+
+  var deleteSelectedItems = function () {
+    if (!selectedItems.length) return
+    var $grid = $('#jsgrid-delete')
+    $grid.jsGrid('option', 'pageIndex', 1)
+    $grid.jsGrid('loadData')
+    selectedItems = []
+  }
 }())
