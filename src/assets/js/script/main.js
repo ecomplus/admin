ae60ff40a39a017fe9611112958627c7c5a2a797
Leonardo Matos <leomp120894@gmail.com>
2018-05-14 12:33:34 -0300

    get tab JSON data globally

--- src/assets/js/script/main.js
+++ src/assets/js/script/main.js
@@ -457,8 +457,9 @@ app.ready(function () {
     window.callApi = callApi
     // store data when necessary
     // commit changes on tab data globally
+    // get tab JSON data globally
     // improve reactivity
-    window.tabData = window.tabCommit = {}
+    window.tabData = window.tabCommit = window.tabLoad = {}
 
     // general function to load HTML content
     window.loadContent = function (uri, el) {
