a61650f1734dfcfb5a7301419cb1976fe7c59218
Leonardo Matos <leomp120894@gmail.com>
2018-05-27 00:51:32 -0300

    forms base js

---
+++ src/assets/js/routes/resources/form/_base.js
@@ -0,0 +1,20 @@
+/*!
+ * Copyright 2018 E-Com Club
+ */
+
+(function () {
+  'use strict'
+
+  // current tab ID
+  var tabId = window.tabId
+  var elContainer = $('#' + tabId + '-tab-normal')
+  // prefix tab ID on content elements IDs
+  window.renderContentIds(elContainer)
+  window.setSaveAction()
+
+  $('#' + tabId + '-html-editor').summernote()
+  $('select[multiple]').tagsinput('items')
+  $('select:not([multiple])').selectpicker({
+    'style': 'btn-light'
+  })
+}())
