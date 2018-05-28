7432b0ec053efe47236a2daa90b2f0f8193cda35
Leonardo Matos <leomp120894@gmail.com>
2018-05-28 15:28:02 -0300

    commit data changes from form

--- src/assets/js/routes/resources/form.js
+++ src/assets/js/routes/resources/form.js
@@ -12,11 +12,26 @@
   window.renderContentIds(elContainer)
   window.setSaveAction()
 
+  // JSON document
+  var data = window.tabData[tabId]
+  var commit = window.tabCommit[tabId]
+
   var $form = elContainer.children('form')
 
   $form.find('.html-editor').summernote()
   $form.find('select[multiple]').tagsinput('items')
   $form.find('select:not([multiple])').selectpicker({
     'style': 'btn-light'
+  })
+
+  $form.find('input[type="text"]').change(function () {
+    var prop = $(this).attr('name')
+    if (prop && prop !== '') {
+      // string property
+      data[prop] = $(this).val()
+      // global object already changed by reference
+      // commit only to perform reactive actions
+      commit(data, true)
+    }
   })
 }())
