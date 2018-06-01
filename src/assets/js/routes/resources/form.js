76b09432755a7b0446e8b73c63ec6a4c0ccdcae8
Leonardo Matos <leomp120894@gmail.com>
2018-06-01 13:06:59 -0300

    fill select with resource list

--- src/assets/js/routes/resources/form.js
+++ src/assets/js/routes/resources/form.js
@@ -47,12 +47,6 @@
     window.callApi(uri, method, callback, Data())
   })
 
-  $form.find('.html-editor').summernote()
-  $form.find('select[multiple]').tagsinput('items')
-  $form.find('select:not([multiple])').selectpicker({
-    'style': 'btn-light'
-  })
-
   $form.find('input[type="text"]').change(function () {
     var prop = $(this).attr('name')
     if (prop && prop !== '') {
@@ -62,6 +56,52 @@
       // global object already changed by reference
       // commit only to perform reactive actions
       commit(data, true)
+    }
+  })
+
+  // count AJAX requests
+  var todo = 0
+  var done = 0
+  var Done = function () {
+    done++
+    if (done === todo) {
+      // ready
+      $form.find('.html-editor').summernote()
+      $form.find('select[multiple]').tagsinput('items')
+      $form.find('select:not([multiple])').selectpicker({
+        'style': 'btn-light'
+      })
+      // show form
+      $form.removeClass('ajax ajax-cards')
+    }
+  }
+
+  // fill select options (autocomplete)
+  $form.find('select').each(function (index) {
+    // resource name
+    var fill = $(this).data('fill')
+    if (fill) {
+      todo++
+      // array of destination elements
+      var $els = $form.find('select[data-fill-same="' + $(this).attr('name') + '"]')
+      $els.push($(this))
+
+      window.callApi(fill + '.json', 'GET', function (err, json) {
+        if (!err) {
+          // response should be a resource list
+          var list = json.result
+          if (list) {
+            for (var i = 0; i < list.length; i++) {
+              var doc = list[i]
+              for (var j = 0; j < $els.length; j++) {
+                // fill select element with new option
+                $els[j].append('<option value="' + doc._id + '">' + doc.name + '</option>')
+              }
+            }
+          }
+        }
+        Done()
+      })
     }
   })
 }())
