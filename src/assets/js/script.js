c123985053a123f866030c5ddee0c1a85baf449f
Leonardo Matos <leomp120894@gmail.com>
2018-07-05 18:21:20 -0300

    reset topbar title with current input val

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -1402,7 +1402,8 @@ app.ready(function () {
               elTitle.change(function () {
                 updateTopbarTitle($(this).val())
               })
-              updateTopbarTitle('')
+              // reset topbar title with current input val
+              updateTopbarTitle(elTitle.val())
               clearTopbarTitle = false
             }
           }
