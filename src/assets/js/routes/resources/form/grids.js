ade52daaeafaaa1285ed3830869fa68bb8d9e2ee
Leonardo Matos <leomp120894@gmail.com>
2018-07-11 15:50:56 -0300

    grids options list, add li element

--- src/assets/js/routes/resources/form/grids.js
+++ src/assets/js/routes/resources/form/grids.js
@@ -11,7 +11,13 @@
   var $list = $('#' + tabId + '-options-list')
   var addOption = function () {
     // add li element
-    $list.append('<li class="list-group-item"><i class="fa fa-cog"></i></li>')
+    var li = '<li class="list-group-item">' +
+               '<div class="input-group">' +
+                 '<input type="text" class="form-control">' +
+                 '<div class="input-group-append"><i class="fa fa-cog"></i></div>' +
+               '</div>' +
+             '</li>'
+    $list.append(li)
   }
 
   $('#' + tabId + '-add-option').click(addOption)
