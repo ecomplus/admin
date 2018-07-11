822aecac07003a615ad93c046e1dbc69ebae5c67
Leonardo Matos <leomp120894@gmail.com>
2018-07-11 17:13:31 -0300

    grids form js: option li element HTML (fix)

--- src/assets/js/routes/resources/form/grids.js
+++ src/assets/js/routes/resources/form/grids.js
@@ -10,25 +10,25 @@
 
   var $list = $('#' + tabId + '-options-list')
   // option li element HTML
-  var $liOption = $('<li />', {
-    html: '<div class="input-group">' +
-            '<div class="input-group-prepend">' +
-              '<span class="input-group-text">' +
-                '<div class="custom-control custom-checkbox">' +
-                  '<input type="checkbox" class="custom-control-input">' +
-                  '<label class="custom-control-label"> </label>' +
-                '</div>' +
-              '</span>' +
-            '</div>' +
-            '<input class="form-control" type="text">' +
-            '<span class="input-group-append">' +
-              '<button class="btn btn-light" type="button"><i class="fa fa-cog"></i></button>' +
-            '</span>' +
-          '</div>'
-  })
+  var liOption = '<div class="input-group">' +
+                   '<div class="input-group-prepend">' +
+                     '<span class="input-group-text">' +
+                       '<div class="custom-control custom-checkbox">' +
+                         '<input type="checkbox" class="custom-control-input">' +
+                         '<label class="custom-control-label"> </label>' +
+                       '</div>' +
+                     '</span>' +
+                   '</div>' +
+                   '<input class="form-control" type="text" />' +
+                   '<span class="input-group-append">' +
+                     '<button class="btn btn-light" type="button"><i class="fa fa-cog"></i></button>' +
+                   '</span>' +
+                 '</div>'
   var addOption = function () {
     // add li element
-    $list.append($liOption)
+    $list.append($('<li />', {
+      html: liOption
+    }))
   }
 
   $('#' + tabId + '-add-option').click(addOption)
