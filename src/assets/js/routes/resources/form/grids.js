19fdb4b44e99409fa7174f664b4ee670a49f3bb2
Leonardo Matos <leomp120894@gmail.com>
2018-07-11 15:05:47 -0300

    minor CSS fixes, grids.js (form)

---
+++ src/assets/js/routes/resources/form/grids.js
@@ -0,0 +1,14 @@
+/*!
+ * Copyright 2018 E-Com Club
+ */
+
+(function () {
+  'use strict'
+
+  // current tab ID
+  var tabId = window.tabId
+
+  $('#' + tabId + '-add-option').click(function () {
+    console.log(1)
+  })
+}())
