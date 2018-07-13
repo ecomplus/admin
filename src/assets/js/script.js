d2eed947c73e839e9c98a0476d7a5b463e6333b0
Leonardo Matos <leomp120894@gmail.com>
2018-07-13 14:26:24 -0300

    handleInput: minor changes

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -350,19 +350,20 @@ app.config({
     return objectIdPad(randomInt(10000, 99999) + '000000000000000000', timestamp)
   }
 
-  window.handleInputs = function ($form, callback) {
+  window.handleInputs = function ($form, inputToData) {
     /*
       default form setup
       handle custom data attributes and callback on change
     */
 
     $form.find('input[type="checkbox"]').change(function () {
-      callback($(this), true)
+      // true for checkbox type
+      inputToData($(this), true)
     })
 
     $form.find('input[type="radio"]').change(function () {
       var $checked = $form.find('input[name="' + $(this).attr('name') + '"]:checked')
-      callback($checked)
+      inputToData($checked)
 
       // check if other elements are controled by this options
       var disable = $checked.data('disable')
@@ -378,7 +379,7 @@ app.config({
     })
 
     $form.find('input[type="text"],select,textarea').change(function () {
-      callback($(this))
+      inputToData($(this))
 
       // check if other input field is filled based on this
       var fillField = $(this).data('fill-field')
