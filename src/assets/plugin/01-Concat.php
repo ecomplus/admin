7412015b5bbcd85cbccd4d07c6437f18486c10df
Leonardo Matos <leomp120894@gmail.com>
2018-07-17 18:03:16 -0300

    not using Cleave (yet)

--- src/assets/plugin/01-Concat.php
+++ src/assets/plugin/01-Concat.php
@@ -4,7 +4,6 @@
 $files = array(
   'md5/md5.min.js',
   'promise/es6-promise.auto.min.js',
-  'cleave/dist/cleave.min.js',
   'maskmoney/dist/jquery.maskMoney.min.js',
   'sortable/sortable.min.js',
   'dialogflow/ApiAi.min.js',
