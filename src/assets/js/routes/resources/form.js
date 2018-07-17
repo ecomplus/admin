8cd95e64663587cfb399da30f8c9db8a4f9da2b5
Leonardo Matos <leomp120894@gmail.com>
2018-07-17 12:43:24 -0300

    changing input type text with data is-number, default decimal

--- src/assets/js/routes/resources/form.js
+++ src/assets/js/routes/resources/form.js
@@ -344,7 +344,7 @@
                       if ($el.data('money')) {
                         $el.val(formatMoney(val))
                       } else {
-                        $el.val(numberToString(val, $el.data('decimal')))
+                        $el.val(numberToString(val, !$el.data('integer')))
                       }
                       break
 
