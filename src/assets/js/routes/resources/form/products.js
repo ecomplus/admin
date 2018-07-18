6861ad8a05186ac2384379226e79c593712d0fb7
Leonardo Matos <leomp120894@gmail.com>
2018-07-18 17:05:31 -0300

    handling new brands and categories on product form

--- src/assets/js/routes/resources/form/products.js
+++ src/assets/js/routes/resources/form/products.js
@@ -19,18 +19,40 @@
     Tab.formSetup()
 
     setTimeout(function () {
+      // generate IDs for each new option
+      var idPad = randomObjectId()
+      var index = 0
+
+      var addOption = function ($select, term) {
+        // add option to select
+        var $option = $('<option />', {
+          text: term,
+          selected: true,
+          value: JSON.stringify({
+            _id: objectIdPad(idPad, '' + index),
+            name: term
+          })
+        })
+        // then refresh the picker plugin
+        // trigger change to handle commit and save action
+        $select.append($option).selectpicker('refresh').trigger('change')
+        index++
+      }
+
       $form.find('select[name="brands"],select[name="categories"]').each(function () {
         var $dropdown = $(this).prev('.dropdown-menu')
         if ($dropdown.length) {
           var $select = $(this)
 
+          // listen keydown event on search input
+          // detect when search have no results
           $dropdown.find('.bs-searchbox input').keydown(function () {
             var $input = $(this)
+            // wait for search processing
             setTimeout(function () {
               var $li = $dropdown.find('.dropdown-menu > .no-results')
               if ($li.length) {
                 // no results
-                var term = $input.val()
                 // create 'add option' button
                 var $btn = $('<button />', {
                   'class': 'btn btn-outline btn-secondary btn-sm btn-block',
@@ -40,12 +62,12 @@
                     'pt_br': 'Adicionar'
                   }),
                   click: function () {
-                    $select.append('<option value="1" selected >' + term + '</option>').selectpicker('refresh')
+                    addOption($select, $input.val())
                   }
                 })
-                $li.html($btn).fadeIn()
+                $li.html($btn)
               }
-            }, 200)
+            }, 300)
           })
         }
       })
