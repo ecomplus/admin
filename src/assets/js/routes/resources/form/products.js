c3b892b27b99bd7c8ff92294eab455057d280ef7
Leonardo Matos <leomp120894@gmail.com>
2018-07-21 14:42:51 -0300

    global substring matcher function

--- src/assets/js/routes/resources/form/products.js
+++ src/assets/js/routes/resources/form/products.js
@@ -219,24 +219,6 @@
       'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
       'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
 
-    var substringMatcher = function (strs) {
-      return function findMatches (q, cb) {
-        var matches, substrRegex
-        // an array that will be populated with substring matches
-        matches = []
-        // regex used to determine if a string contains the substring `q`
-        substrRegex = new RegExp(q, 'i')
-        // iterate through the pool of strings and for any string that
-        // contains the substring `q`, add it to the `matches` array
-        $.each(strs, function (i, str) {
-          if (substrRegex.test(str)) {
-            matches.push(str)
-          }
-        })
-        cb(matches)
-      }
-    }
-
     var $inputSku = $form.find('input[name="sku"]')
     $inputSku.click(function () {
       // select all input text
