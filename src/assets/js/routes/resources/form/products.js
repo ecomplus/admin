1df0724dab5e8202e3bf29f146d4e69d4aff1aa5
Leonardo Matos <leomp120894@gmail.com>
2018-07-21 13:59:47 -0300

    product form: setup typeahead for grid inputs

--- src/assets/js/routes/resources/form/products.js
+++ src/assets/js/routes/resources/form/products.js
@@ -141,6 +141,15 @@
         }
       })
 
+      $inputGrid.typeahead({
+        hint: true,
+        highlight: true,
+        minLength: 1
+      }, {
+        name: 'states',
+        source: substringMatcher(states)
+      })
+
       // setup remove button
       $li.find('.remove-grid').click(function () {
         removeGrid($li)
@@ -197,6 +206,34 @@
         // all grids removed
         // list is empty, hide list header
         $('#' + tabId + '-grids-list-header, #' + tabId + '-add-option-header').slideUp()
+      }
+    }
+
+    var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+      'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+      'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+      'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+      'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+      'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+      'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+      'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+      'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming']
+
+    var substringMatcher = function (strs) {
+      return function findMatches (q, cb) {
+        var matches, substrRegex
+        // an array that will be populated with substring matches
+        matches = []
+        // regex used to determine if a string contains the substring `q`
+        substrRegex = new RegExp(q, 'i')
+        // iterate through the pool of strings and for any string that
+        // contains the substring `q`, add it to the `matches` array
+        $.each(strs, function (i, str) {
+          if (substrRegex.test(str)) {
+            matches.push(str)
+          }
+        })
+        cb(matches)
       }
     }
 
