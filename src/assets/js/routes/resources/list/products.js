0da5c445b8dce0681e676409505f9f19ca7af430
Leonardo Matos <leomp120894@gmail.com>
2018-08-08 16:21:11 -0300

    products list: performing search body (query)

--- src/assets/js/routes/resources/list/products.js
+++ src/assets/js/routes/resources/list/products.js
@@ -15,13 +15,6 @@
   // var lang = window.lang
   var i18n = window.i18n
 
-  // search input
-  var $search = $('#search-products')
-  $search.find('input').attr('placeholder', i18n({
-    'en_us': 'Polo Shirt',
-    'pt_br': 'Camisa Polo'
-  }))
-
   // resource list data
   var data, list
   var updateData = function () {
@@ -32,42 +25,91 @@
       })
     })
   }
+  // startup
   updateData()
 
   if (list.length) {
-    // update list content
-    var $list = $('#products-list-results')
-    // generate grid rows
-    var $rows = []
-    var $rowItems = []
-    var addRow = function () {
-      $rows.push($('<div />', {
-        'class': 'row row-products',
-        html: $rowItems
-      }))
-    }
-
-    for (var i = 0; i < list.length; i++) {
-      var item = list[i]
-      var $item = $('<div />', {
-        'class': 'col',
-        html: '<div class="text-monospace">' + item.sku + '</div>' + item.name
-      })
-
-      // count number of items on current row
-      $rowItems.push($item)
-      if ($rowItems.length === 5) {
-        addRow()
-        // reset
-        $rowItems = []
+    var updateContent = function () {
+      // update list content
+      var $list = $('#products-list-results')
+      // generate grid rows
+      var $rows = []
+      var $rowItems = []
+      var addRow = function () {
+        $rows.push($('<div />', {
+          'class': 'row row-products',
+          html: $rowItems
+        }))
       }
+
+      for (var i = 0; i < list.length; i++) {
+        var item = list[i]
+        var $item = $('<div />', {
+          'class': 'col',
+          html: '<div class="text-monospace">' + item.sku + '</div>' + item.name
+        })
+
+        // count number of items on current row
+        $rowItems.push($item)
+        if ($rowItems.length === 5) {
+          addRow()
+          // reset
+          $rowItems = []
+        }
+      }
+
+      if ($rowItems.length) {
+        // last incomplete row
+        addRow()
+      }
+      // fill products list content
+      $list.html($rows)
     }
 
-    if ($rowItems.length) {
-      // last incomplete row
-      addRow()
+    // JSON data load
+    var load = function (Query) {
+      // update search query
+      query = Object.assign(query, Query)
+      // run a new search request and update data
+      var callback = function () {
+        updateData()
+        updateContent()
+      }
+      Tab.load(callback, query)
     }
-    // fill products list content
-    $list.html($rows)
+    // Elasticsearch query object
+    var query = {}
+
+    // search form
+    var $search = $('#search-products')
+    var $searchInput = $search.find('input')
+    $searchInput.attr('placeholder', i18n({
+      'en_us': 'Polo Shirt',
+      'pt_br': 'Camisa Polo'
+    }))
+    $search.submit(function () {
+      // https://ecomsearch.docs.apiary.io/#reference/items/items-search/complex-search
+      load({
+        query: {
+          bool: {
+            must: {
+              multi_match: {
+                query: $searchInput.val().trim(),
+                fields: [
+                  'name',
+                  'keywords'
+                ]
+              }
+            }
+          }
+        }
+      })
+    })
+
+    // ready
+    // show first results
+    updateContent()
+  } else {
+    // no products
   }
 }())
