0fc77f03c017c9c1311f6a50649d1cb21eadd9e4
Leonardo Matos <leomp120894@gmail.com>
2018-08-08 12:58:20 -0300

    products list: search input

--- src/assets/js/routes/resources/list/products.js
+++ src/assets/js/routes/resources/list/products.js
@@ -13,11 +13,12 @@
   window.renderContentIds(elContainer)
 
   // var lang = window.lang
-  // var i18n = window.i18n
+  var i18n = window.i18n
 
-  // create button
-  $('#t' + tabId + '-create').click(function () {
-    // go to 'new' route
-    window.location = '/' + window.location.hash + '/new'
-  })
+  // search input
+  var $search = $('#search-products')
+  $search.find('input').attr('placeholder', i18n({
+    'en_us': 'Polo Shirt',
+    'pt_br': 'Camisa Polo'
+  }))
 }())
