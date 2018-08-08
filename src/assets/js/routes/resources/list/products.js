ff309bf7bed58a8ee4a190a9f0fae0452966d8bd
Leonardo Matos <leomp120894@gmail.com>
2018-08-08 15:54:18 -0300

    products list: rendering grid

--- src/assets/js/routes/resources/list/products.js
+++ src/assets/js/routes/resources/list/products.js
@@ -7,7 +7,7 @@
 
   // current tab ID
   var tabId = window.tabId
-  // var Tab = window.Tabs[tabId]
+  var Tab = window.Tabs[tabId]
   var elContainer = $('#t' + tabId + '-tab-normal')
   // prefix tab ID on content elements IDs
   window.renderContentIds(elContainer)
@@ -21,4 +21,53 @@
     'en_us': 'Polo Shirt',
     'pt_br': 'Camisa Polo'
   }))
+
+  // resource list data
+  var data, list
+  var updateData = function () {
+    data = Tab.data
+    list = data.hits.hits.map(function (item) {
+      return Object.assign(item._source, {
+        _id: item._id
+      })
+    })
+  }
+  updateData()
+
+  if (list.length) {
+    // update list content
+    var $list = $('#products-list-results')
+    // generate grid rows
+    var $rows = []
+    var $rowItems = []
+    var addRow = function () {
+      $rows.push($('<div />', {
+        'class': 'row row-products',
+        html: $rowItems
+      }))
+    }
+
+    for (var i = 0; i < list.length; i++) {
+      var item = list[i]
+      var $item = $('<div />', {
+        'class': 'col',
+        html: '<div class="text-monospace">' + item.sku + '</div>' + item.name
+      })
+
+      // count number of items on current row
+      $rowItems.push($item)
+      if ($rowItems.length === 5) {
+        addRow()
+        // reset
+        $rowItems = []
+      }
+    }
+
+    if ($rowItems.length) {
+      // last incomplete row
+      addRow()
+    }
+    // fill products list content
+    $list.html($rows)
+  }
 }())
