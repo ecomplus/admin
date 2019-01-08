9f162ae76e96bff3579c85356042398877a93b1e
Leonardo Matos <leomp120894@gmail.com>
2019-01-08 16:37:10 -0200

    handle manual action topbar toggle

--- docs/assets/js/script.js
+++ docs/assets/js/script.js
@@ -216,6 +216,23 @@ app.config({
       windowPadding: 70
     })
 
+    // handle manual action topbar toggle
+    $('#topbar-action-toggle').click(function () {
+      var $topbar = $('#topbar-action')
+      if (!$topbar.hasClass('h-auto')) {
+        // hide
+        $topbar.slideUp(200, function () {
+          $(this).find('#topbar-action-body').addClass('hidden')
+          $(this).addClass('h-auto').fadeIn()
+        })
+      } else {
+        // show
+        $topbar.hide().removeClass('h-auto').slideDown(200, function () {
+          $(this).find('#topbar-action-body').hide().removeClass('hidden').fadeIn()
+        })
+      }
+    })
+
     /* jQuery addons */
 
     // https://gist.github.com/beiyuu/2029907
