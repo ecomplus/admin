1a2d2b17e27084a4dde09b71d4f53f029dab1dc7
Leonardo Matos <leomp120894@gmail.com>
2019-02-04 20:03:18 -0200

    recalculate order total value with amount fields

--- docs/assets/js/routes/resources/form/orders.js
+++ docs/assets/js/routes/resources/form/orders.js
@@ -19,8 +19,26 @@
   var data = Data()
   var $orderBase = $('#t' + tabId + '-order-base')
 
+  // watch amount values
+  var $amount = $orderBase.find('#t' + tabId + '-order-amount')
+  var $total = $amount.find('input[name="amount.total"]')
+  $amount.find('input:not([name="amount.total"])').change(function () {
+    setTimeout(function () {
+      // recalculate order total value
+      var amount = Data().amount
+      var total = (amount.subtotal || 0) + (amount.freight || 0) - (amount.discount || 0)
+      if (amount.tax) {
+        total += amount.tax
+      }
+      if (amount.extra) {
+        total += amount.extra
+      }
+      $total.val(formatMoney(total)).trigger('change')
+    }, 150)
+  })
+
   // handle amount extra fields collapse
-  var $amountExtra = $orderBase.find('#t' + tabId + '-extra-amount')
+  var $amountExtra = $amount.find('#t' + tabId + '-extra-amount')
   var toggleAmountExtra = function () {
     $amountExtra.children('div').slideToggle()
   }
