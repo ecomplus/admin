77f3ec7e53bc679186c75a94b41e6acd5097923d
Leonardo Matos <leomp120894@gmail.com>
2019-02-05 12:17:05 -0200

    rendering order buyers on blocks

--- docs/assets/js/routes/resources/form/orders.js
+++ docs/assets/js/routes/resources/form/orders.js
@@ -9,7 +9,7 @@
   var tabId = window.tabId
   var Tab = window.Tabs[tabId]
   // edit JSON document
-  // var commit = Tab.commit
+  var commit = Tab.commit
   var Data = function () {
     // current data from global variable
     return Tab.data
@@ -50,54 +50,86 @@
     }
   }
 
-  // render buyers block
-  var $buyers = $orderBase.find('#t' + tabId + '-order-buyers')
-  var $buyerInfo = $buyers.find('#t' + tabId + '-buyer-info')
+  // render buyers blocks
+  var $buyers, $buyerInfo
+  // BUG: sometimes $buyers and $buyerInfo was empty
+  // FIX: delay to fix bug
+  setTimeout(function () {
+    $buyers = $orderBase.find('#t' + tabId + '-order-buyers')
+    $buyerInfo = $buyers.find('#t' + tabId + '-buyer-info')
 
-  var showBuyer = function (index) {
-    // show specific buyer info from buyers list
-    $buyerInfo.slideUp(400, function () {
-      var buyer = Data().buyers[index]
-      // basic info only: email, name, phone numbers
-      var html = ''
-      if (buyer.main_email) {
-        html += '<a href="mailto:' + buyer.main_email + '" target="_blank">' + buyer.main_email + '</a>'
+    // show current buyers from list if any
+    if (data.buyers) {
+      for (var i = 0; i < data.buyers.length; i++) {
+        showBuyer(data.buyers[i])
       }
+    }
+  }, 200)
 
-      // full name and nickname
-      var name
-      if (buyer.name) {
-        name = buyer.name.given_name
-        if (buyer.name.middle_name) {
-          name += ' ' + buyer.name.middle_name
-        }
-        if (buyer.name.family_name) {
-          name += ' ' + buyer.name.family_name
-        }
-        if (buyer.display_name) {
-          name += ' (' + buyer.display_name + ')'
-        }
-      } else if (buyer.display_name) {
-        name = buyer.display_name
-      }
-      if (name) {
-        html += '<br>' + name
-      }
+  var showBuyer = function (buyer) {
+    // show specific buyer info
+    var objectId = buyer._id
+    // basic info only: email, name, phone numbers
+    var html = ''
+    if (buyer.main_email) {
+      html += '<a href="mailto:' + buyer.main_email + '" target="_blank">' + buyer.main_email + '</a>'
+    }
 
-      // render list of phone numbers
-      if (buyer.phones) {
-        for (var i = 0; i < buyer.phones.length; i++) {
-          html += '<br><span class="text-muted">' + buyer.phones[i].number + '</span>'
+    // full name and nickname
+    var name
+    if (buyer.name) {
+      name = buyer.name.given_name
+      if (buyer.name.middle_name) {
+        name += ' ' + buyer.name.middle_name
+      }
+      if (buyer.name.family_name) {
+        name += ' ' + buyer.name.family_name
+      }
+      if (buyer.display_name) {
+        name += ' (' + buyer.display_name + ')'
+      }
+    } else if (buyer.display_name) {
+      name = buyer.display_name
+    }
+    if (name) {
+      html += '<br>' + name
+    }
+
+    // render list of phone numbers
+    if (buyer.phones) {
+      for (var i = 0; i < buyer.phones.length; i++) {
+        html += '<br><span class="text-muted">' + buyer.phones[i].number + '</span>'
+      }
+    }
+
+    // icon to handle buyer delete from list
+    var $remove = $('<i>', {
+      'class': 'py-10 pr-10 remove fa fa-trash'
+    }).click(function () {
+      var data = Data()
+      var buyers = data.buyers
+      for (var i = 0; i < buyers.length; i++) {
+        var buyer = buyers[i]
+        if (buyer._id === objectId) {
+          // remove buyer from list
+          buyers.splice(i, 1)
+          // commit only to perform reactive actions
+          commit(data, true)
+          break
         }
       }
-
-      // update block HTML and show again
-      $(this).html(html).slideDown()
+      // remove block
+      $div.slideUp(300, function () {
+        $(this).remove()
+      })
     })
-  }
 
-  // show the first buyer if any
-  if (data.buyers && data.buyers.length) {
-    showBuyer(0)
+    // create buyer block, append and show
+    var $div = $('<div>', {
+      'class': 'hidden mb-3',
+      html: [ $remove, html ]
+    })
+    $buyerInfo.append($div)
+    $div.slideDown()
   }
 }())
