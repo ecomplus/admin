25bd231aff4f0896eb5b2a06c755a741ad5b3660
Leonardo Matos <leomp120894@gmail.com>
2019-03-11 18:23:39 -0300

    fix: minor fix for toggleBlocksByValue global function

--- src/assets/js/script.js
+++ src/assets/js/script.js
@@ -568,7 +568,7 @@ app.config({
     var currentValue = $ref.val()
     $form.find('div[data-update="' + $ref.attr('name') + '"] > *').each(function () {
       var refValue = $(this).data('value')
-      if (refValue === currentValue) {
+      if (!refValue || refValue === currentValue) {
         $(this).slideDown()
       } else {
         // hide block
