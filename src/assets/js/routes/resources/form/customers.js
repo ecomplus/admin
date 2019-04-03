c82a41683eac5bbccd2cabfea349b6820b082dbc
Matheus Reis <matheusreis@matheuss-mbp.localdomain>
2019-04-02 21:05:57 -0300

    fix: fix erro at customers's admin and update birthdate

--- src/assets/js/routes/resources/form/customers.js
+++ src/assets/js/routes/resources/form/customers.js
@@ -17,38 +17,50 @@
 
   // render cart items on table
   var setup = function () {
-    $('input[type=radio][name=registry_type]').change(function () {
-      if (this.value === 'p') {
-        $('.cpf').show()
-        $('.cnpj').hide()
-      } else if (this.value === 'j') {
-        $('.cpf').hide()
-        $('.cnpj').show()
-      }
-    })
-    $('input[type=radio][name=inscription_type]').change(function () {
-      if (this.value === 'State') {
-        $('.state').show()
-        $('.municipal').hide()
-      } else if (this.value === 'Municipal') {
-        $('.state').hide()
-        $('.municipal').show()
-      }
-    })
-  }
-  // mask input
-  var toggleBlocksByValue = function ($form) {
-    $form.find('input[data-mask]').each(function () {
-      switch ($(this).data('mask')) {
-        case 'cpf':
-          $(this).inputmask('999.999.999-99')
-          break
+    var $infoCustomer = $('#t' + tabId + '-info-customer')
+    var $inputRegister = $infoCustomer.find('#t' + tabId + '-registry-type')
+    var $registryType = $inputRegister.find('input[name="registry_type"]')
+    var $physical = $infoCustomer.find('#t' + tabId + '-physical')
+    var $juridical = $infoCustomer.find('#t' + tabId + '-juridical')
+    var $inscriptionType = $infoCustomer.find('#t' + tabId + '-inscription-number')
+    var $birth = $infoCustomer.find('#t' + tabId + '-birth-date')
+    var $birthDate = $birth.find('input[name="birth_date"]')
 
-        case 'cnpj':
-          $(this).inputmask('99.999.999/9999-99')
-          break
+    $registryType.change(function () {
+      if (this.value === 'p') {
+        $physical.show()
+        $juridical.hide()
+        $inscriptionType.hide()
+      } else {
+        $physical.hide()
+        $juridical.show()
+        $inscriptionType.show()
       }
     })
+
+    // birth date treatement of data
+    $birthDate.change(function () {
+      console.log($birthDate.val())
+      var $newDate = $birthDate.val().replace(/(\d{2})(\d{2})(\d{4})/, '$1.$2.$3')
+      console.log($newDate)
+      var $date = parseInt($newDate.split('.'))
+      console.log($date)
+    })
+
+    // mask input
+    var toggleBlocksByValue = function ($form) {
+      Tab.$form.find('input[data-mask]').each(function () {
+        switch ($(this).data('mask')) {
+          case 'cpf':
+            $(this).inputmask('999.999.999-99')
+            break
+
+          case 'cnpj':
+            $(this).inputmask('99.999.999/9999-99')
+            break
+        }
+      })
+    }
   }
 
   // wait for the form to be ready
