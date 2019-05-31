0facabdee0fefa2be6e856b48588b6b1471572b1
Leonardo Matos <leomp120894@gmail.com>
2019-05-31 12:35:45 -0300

    feat: setting up dashboard sidebar and header

--- src/components/_EcComponent/App.vue
+++ src/components/_EcComponent/App.vue
@@ -1,3 +1,7 @@
+<template>
+  <div class="ec-component">
+  </div>
+</template>
+
 <script src="./index.js"></script>
 <style lang="scss" src="./styles.scss"></style>
-<template lang="html" src="./template.html"></template>
--- src/components/templates/EcDashboard/index.js
+++ src/components/templates/EcDashboard/index.js
@@ -1,9 +1,11 @@
 import EcHeader from '@/components/organisms/EcHeader/App.vue'
+import EcSidebar from '@/components/organisms/EcSidebar/App.vue'
 
 export default {
   name: 'EcDashboard',
 
   components: {
-    EcHeader
+    EcHeader,
+    EcSidebar
   }
 }
