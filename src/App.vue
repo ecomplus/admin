2bd33e8b603ff30cce0c9cd75fc35f7100785094
Leonardo Matos <leomp120894@gmail.com>
2019-05-31 13:04:23 -0300

    Revert "chore: setup some dependencies and first component"
    This reverts commit cc02baff9f0537c2846061fe39fcc8ebac745193.

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,29 @@
 <template>
   <div id="app">
-    <RouterView />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view/>
   </div>
 </template>
 
-<style lang="scss" src="./scss/styles.scss"></style>
+<style lang="scss">
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+#nav {
+  padding: 30px;
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
+}
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,15 +1,18 @@
 <template>
   <div class="home">
-    <EcDashboard />
+    <img alt="Vue logo" src="../assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/>
   </div>
 </template>
 
 <script>
-import EcDashboard from '@/components/templates/EcDashboard.vue'
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
 export default {
   name: 'home',
   components: {
-    EcDashboard
+    HelloWorld
   }
 }
 </script>
