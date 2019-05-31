f4a58f6f2c75ddd599ad6e89ba1f626372d8bd1d
Leonardo Matos <leomp120894@gmail.com>
2019-05-31 12:35:45 -0300

    feat: setting up dashboard sidebar and header

--- src/components/templates/EcDashboard/App.vue
+++ src/components/templates/EcDashboard/App.vue
@@ -1,3 +1,13 @@
+<template>
+  <div class="ec-dashboard">
+    <aside class="ec-dashboard__aside">
+      <EcSidebar />
+    </aside>
+    <main class="ec-dashboard__main">
+      <EcHeader />
+    </main>
+  </div>
+</template>
+
 <script src="./index.js"></script>
 <style lang="scss" src="./styles.scss"></style>
-<template lang="html" src="./template.html"></template>
