dbcadd0bf79eb38d1613a0fe60797326fe39e7cd
Leonardo Matos <leomp120894@gmail.com>
2019-05-31 13:03:44 -0300

    Revert "feat: setting up dashboard sidebar and header"
    This reverts commit f4a58f6f2c75ddd599ad6e89ba1f626372d8bd1d.

--- src/components/_EcComponent/App.vue
+++ src/components/_EcComponent/App.vue
@@ -1,7 +1,3 @@
-<template>
-  <div class="ec-component">
-  </div>
-</template>
-
 <script src="./index.js"></script>
 <style lang="scss" src="./styles.scss"></style>
+<template lang="html" src="./template.html"></template>
--- src/components/templates/EcDashboard/App.vue
+++ src/components/templates/EcDashboard/App.vue
@@ -1,13 +1,3 @@
-<template>
-  <div class="ec-dashboard">
-    <aside class="ec-dashboard__aside">
-      <EcSidebar />
-    </aside>
-    <main class="ec-dashboard__main">
-      <EcHeader />
-    </main>
-  </div>
-</template>
-
 <script src="./index.js"></script>
 <style lang="scss" src="./styles.scss"></style>
+<template lang="html" src="./template.html"></template>
