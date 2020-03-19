e4b569da1a3b98671e90c786e69ebd3e90bb1a17
Matheus <matheus@e-com.club>
2020-03-18 21:12:29 -0300

    chore: delete console logs

--- src/assets/js/routes/resources/list.js
+++ src/assets/js/routes/resources/list.js
@@ -635,6 +635,13 @@
                 }
               }
             }
+            if (fieldOpts.templates) {
+              fieldObj.itemTemplate = function (item) {
+                if (item) {
+                  return item
+                }
+              }
+            }
             switch (fieldOpts.type) {
               case 'link':
                 fieldObj.itemTemplate = function (text) {
@@ -930,11 +937,9 @@
                 } else {
                   // handle bulk items edit
                   var data = {}
-                  console.log(bulkEditFields)
                   for (var i = 0; i < bulkEditFields.length; i++) {
                     field = bulkEditFields[i]
                     if (query[field] !== '') {
-                      console.log(query[field])
                       // parse boolean values
                       switch (query[field]) {
                         case 'true':
