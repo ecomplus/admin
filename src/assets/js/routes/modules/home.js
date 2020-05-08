ffe2c11581079a0d52bb3b06a320ddc23707d5c8
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2020-05-07 23:27:42 -0300

    refactor: adjusted home route

--- src/assets/js/routes/modules/home.js
+++ src/assets/js/routes/modules/home.js
@@ -1,7 +1,11 @@
 import * as html from '~/views/home.html'
+import * as form from '../../controllers/resources/form'
+import * as htmlForm from '../../controllers/resources/form/home'
 
 export const load = async (el) => {
   el.html(html)
+  form.handleForm()
+  htmlForm.handleForm()
   await import('../../controllers/resources/form')
   await import('../../controllers/resources/form/home.js')
 }
