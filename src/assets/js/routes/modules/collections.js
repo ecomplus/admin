a944b0e1416b9abaefbaa99117bdbffc129feb94
Thiago Abreu Rodrigues <thiagoabreu.dev@gmail.com>
2020-05-07 23:13:06 -0300

    refactor: add collections route

---
+++ src/assets/js/routes/modules/collections.js
@@ -0,0 +1,19 @@
+import collectionsFormHTML from '~/views/resources/form/collections.html'
+import Resources from '../../controllers/resources'
+import { handleList } from '../../controllers/resources/list'
+import * as form from '../../controllers/resources/form'
+import * as collectionsForm from '../../controllers/resources/form/collections'
+
+class CollectionsResource extends Resources {
+  constructor(resourceEl) {
+    super(resourceEl)
+    this.formHTML = collectionsFormHTML
+    this.listLoaders = [handleList]
+    this.formLoaders = [
+      form.handleForm,
+      collectionsForm.handleForm
+    ]
+  }
+}
+
+export default CollectionsResource
