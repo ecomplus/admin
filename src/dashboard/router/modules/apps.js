b4b165ab1a445e612d73406c6cc1d21741605fba
Leonardo Matos <leomp120894@gmail.com>
2020-05-20 18:15:26 -0300

    fix(apps): importing admin-marketplace source files
    https://github.com/ecomplus/admin-marketplace/issues/102

--- src/dashboard/router/modules/apps.js
+++ src/dashboard/router/modules/apps.js
@@ -1,5 +1,6 @@
 import * as html from '@/dashboard/views/apps.html'
 
-export const load = async (el) => {
+export const load = el => {
   el.html(html)
+  import('@ecomplus/admin-marketplace/src/main').catch(console.error)
 }
