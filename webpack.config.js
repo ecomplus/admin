a95ff264f7cab490617454b106584d2e1ae9d37a
Leonardo Matos <leomp120894@gmail.com>
2020-05-20 01:56:31 -0300

    build(webpack): setup pwa properly, fix output filenames (hash)
    using html plugin

--- webpack.config.js
+++ webpack.config.js
@@ -1,86 +1,196 @@
+'use strict'
+
 const path = require('path')
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const WebpackPwaManifest = require('webpack-pwa-manifest')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 const TerserPlugin = require('terser-webpack-plugin')
-const { InjectManifest } = require('workbox-webpack-plugin');
 
-let swSrc = path.resolve(__dirname, 'public/sw.js')
+const devMode = process.env.NODE_ENV !== 'production'
 
-let plugins = [
-  new VueLoaderPlugin(),
-  new CopyPlugin([
-    { from: './index.html', to: path.resolve(__dirname, 'dist/') },
-    { from: './public', to: path.resolve(__dirname, 'dist/public') },
-    { from: './externals', to: path.resolve(__dirname, 'dist/assets') },
-    { from: './src/pages', to: path.resolve(__dirname, 'dist/public') },
-    { from: './src/routes', to: path.resolve(__dirname, 'dist/public') },
-    { from: './src/assets/', to: path.resolve(__dirname, 'dist/assets') },
-    { from: './src/json', to: path.resolve(__dirname, 'dist/json') },
-  ])
-]
+const dirBase = process.cwd()
+const dirSrc = path.resolve(dirBase, 'src')
+const dirScss = path.resolve(dirBase, 'scss')
+const dirPublic = path.resolve(dirBase, 'public')
+const dirImg = path.resolve(dirPublic, 'img')
+const dirModules = path.resolve(process.cwd(), 'node_modules')
+const dirOutput = path.resolve(process.cwd(), !devMode ? 'dist' : '.serve')
 
-if (process.env.NODE_ENV === 'production') {
-  plugins.push(new MiniCssExtractPlugin())
-  plugins.push(new InjectManifest({ swSrc, swDest: 'sw.js' }))
+const filenameSchema = '[name].[contenthash]'
+
+const entry = {
+  admin: [path.resolve(dirScss, 'styles.scss')]
 }
+if (!devMode) {
+  entry.admin.push(path.resolve(dirSrc, 'starter.js'))
+}
+entry.admin.push(path.resolve(dirSrc, 'index.js'))
 
-module.exports = {
-  mode: process.env.NODE_ENV || 'development',
-  entry: './src/assets/js/script/main.js',
+const config = {
+  mode: devMode ? 'development' : 'production',
+  stats: {
+    colors: true
+  },
+  devtool: 'source-map',
+  performance: {
+    hints: devMode ? false : 'warning',
+    maxEntrypointSize: 1000000,
+    maxAssetSize: 2000000
+  },
+  resolve: {
+    alias: {
+      '@': dirSrc
+    }
+  },
+
+  entry,
   output: {
-    filename: 'assets/js/script.min.js',
-    path: path.resolve(__dirname, 'dist'),
-    publicPath: '/'
+    path: dirOutput,
+    publicPath: '/',
+    filename: devMode ? '[name].js' : `${filenameSchema}.js`,
+    chunkFilename: '[contenthash].js'
   },
   optimization: {
     minimizer: [new TerserPlugin()]
   },
+
   devServer: {
-    contentBase: [path.resolve(__dirname, 'dist/public'), path.join(__dirname, 'dist/assets')],
-    publicPath: '/'
-  },
-  resolve: {
-    alias: {
-      '~': path.resolve(__dirname, 'src')
+    port: 9016,
+    open: true,
+    contentBase: [dirPublic],
+    publicPath: '/',
+    historyApiFallback: {
+      rewrites: [
+        { from: /\/login(\.html)?$/, to: '/' }
+      ]
     }
   },
+
+  plugins: [
+    new HtmlWebpackPlugin(),
+
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : `${filenameSchema}.css`,
+      chunkFilename: '[contenthash].css',
+      ignoreOrder: true
+    }),
+
+    new CleanWebpackPlugin({
+      cleanStaleWebpackAssets: false
+    }),
+
+    new CopyPlugin([{
+      from: dirPublic,
+      to: dirOutput
+    }])
+  ],
+
   module: {
     rules: [
       {
-        test: /\.vue$/,
-        loader: 'vue-loader'
+        test: /\.s?css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true
+            }
+          },
+
+          {
+            loader: 'postcss-loader',
+            options: {
+              ident: 'postcss',
+              minimize: !devMode,
+              plugins: [
+                require('autoprefixer')(),
+                require('cssnano')({ preset: 'default' })
+              ],
+              sourceMap: true
+            }
+          },
+
+          {
+            loader: 'sass-loader',
+            options: {
+              sassOptions: {
+                includePaths: [
+                  dirModules
+                ],
+                importer (file, prev, done) {
+                  if (file.startsWith('~')) {
+                    return done({ file: file.substr(1) })
+                  }
+                  done({ file })
+                }
+              },
+              sourceMap: true
+            }
+          }
+        ]
       },
+
       {
-        test: /\.js$/,
-        exclude: [
-          /node_modules/,
-        ],
+        test: /^(.(?!\.min\.js$))+\.m?js$/,
+        exclude: /node_modules(?!\/@ecomplus\/[^/]+\/(?!dist))/,
         use: {
-          loader: 'babel-loader'
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              ['@babel/preset-env', {
+                useBuiltIns: 'usage',
+                corejs: '3.6',
+                modules: false
+              }]
+            ],
+            plugins: [
+              '@babel/plugin-syntax-dynamic-import'
+            ]
+          }
         }
       },
+
       {
-        test: /\.css$/,
-        use: [MiniCssExtractPlugin.loader, 'style-loader', 'css-loader', 'postcss-loader']
+        test: /^(?!.*\/?html\/.*$).*\.html$/,
+        loader: 'html-loader'
       },
-      {
-        test: /\.s[ac]ss$/i,
-        use: [
-          'style-loader',
-          'css-loader',
-          'sass-loader',
-        ],
-      },
+
       {
         test: /\.(png|woff|woff2|eot|ttf|svg|otf)$/,
-        loader: 'url-loader?limit=100000',
-      },
-      {
-        test: /\.html$/i,
-        loader: 'html-loader',
-      },
+        loader: 'url-loader?limit=100000'
+      }
     ]
-  },
-  plugins
+  }
 }
+
+if (!devMode) {
+  config.plugins.push(
+    new WorkboxPlugin.InjectManifest({
+      swSrc: path.resolve(dirSrc, 'sw.js'),
+      swDest: 'sw.js'
+    }),
+
+    new WebpackPwaManifest({
+      name: 'E-Com Plus Admin',
+      short_name: 'E-Com Plus Admin',
+      description: 'E-Com Plus Cloud Commerce Solution',
+      background_color: '#fafbfb',
+      theme_color: '#37003c',
+      crossorigin: 'use-credentials',
+      icons: [{
+        src: path.resolve(dirImg, 'icon.png'),
+        sizes: [96, 128, 192]
+      }, {
+        src: path.resolve(dirImg, 'large-icon.png'),
+        sizes: [384, 512]
+      }]
+    })
+  )
+}
+
+module.exports = config
