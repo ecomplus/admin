1049f39f59cb7363f62ad87347a54861015289c4
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2020-07-30 19:37:38 -0300

    fix(resources-lists): fix parsing csv booleans (#176)
    * fix(admin-category): add delay in to post mass edit
    
    * fix(spreadsheets-fix-boolean): fixing boolean in spreadsheets
    
    * Update src/controllers/resources.js
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>
    
    * Update src/controllers/resources.js
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>
    
    * chore: removing console log
    
    Co-authored-by: Leonardo Matos <leomp120894@gmail.com>

--- src/controllers/resources.js
+++ src/controllers/resources.js
@@ -590,7 +590,10 @@ export default function () {
                     // fix var type and field name
                     const field = head.replace(/\w+\(([^)]+)\)/i, '$1')
                     row[field] = head.startsWith('Number') ? Number(row[head])
-                      : head.startsWith('Boolean') ? Boolean(row[head]) : row[head]
+                      : head.startsWith('Boolean')
+                        ? typeof row[head] === 'string' ? row[head].toUpperCase().indexOf('TRUE') > -1
+                          : Boolean(row[head])
+                        : row[head]
                     delete row[head]
                   }
                 }
