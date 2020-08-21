e05634a6e0d2aa38b5769ebbe2ea4d3c9f1eb01b
Matheus Reis <35343551+matheusgnreis@users.noreply.github.com>
2020-08-21 13:11:46 -0300

    feat(list-products): also support search by sku (#193)

--- src/controllers/resources/list/products.js
+++ src/controllers/resources/list/products.js
@@ -57,6 +57,7 @@ export default function () {
                   query: term,
                   fields: [
                     'name',
+                    'sku',
                     'keywords'
                   ]
                 }
