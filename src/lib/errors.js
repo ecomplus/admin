e1d8e482f2b49da3ca43ab1dc7f9a69001b84178
Leonardo Matos <leomp120894@gmail.com>
2020-09-01 21:44:48 -0300

    feat(error-handling): better api error msg and try focusing invalid input

--- src/lib/errors.js
+++ src/lib/errors.js
@@ -2,6 +2,8 @@ import { $ecomConfig, i18n } from '@ecomplus/utils'
 import { reload } from './session'
 import toast from './toast'
 
+const { $ } = window
+
 const handleFatalError = err => {
   if (err) {
     console.error(err)
@@ -19,7 +21,18 @@ const handleApiError = data => {
   let msg
   if (typeof data === 'object' && data !== null) {
     if (data.user_message) {
-      msg = data.user_message[$ecomConfig.get('lang')]
+      msg = data.user_message[$ecomConfig.get('lang')].replace(/^data\.?/i, '')
+      if (msg.charAt(0) === ' ') {
+        msg = msg.charAt(1).toUpperCase() + msg.substr(2)
+      }
+      const enMsg = data.user_message.en_us
+      let field = enMsg.replace(/^.*'([\w.]+)'.*$/, '$1')
+      if (field.indexOf(' ') === -1) {
+        if (enMsg.startsWith('data.')) {
+          field = enMsg.replace(/^data.([\w.]+)\s.*$/, '$1') + '.' + field
+        }
+        $(`input[name="${field}"]:visible`).focus()
+      }
     } else if (data.message) {
       msg = data.message
     }
