eec199af31032b96c9c0a36aa9052faabe74b231
Matheus <>
2020-11-30 13:34:04 -0300

    chore: update variables and functions

--- src/controllers/resources/form/home.js
+++ src/controllers/resources/form/home.js
@@ -6,46 +6,46 @@ export default function () {
   'use strict'
   // current tab ID
   const { $, localStorage, tabId, Chart } = window
-  var Tab = window.Tabs[tabId]
+  const Tab = window.Tabs[tabId]
   // render cart items on table
-  var setup = function () {
-    var appTab = $('#app-tab-' + tabId)
-    var countPending = 0
-    var countPaid = 0
-    var countCancelled = 0
-    var $cardAmount = appTab.find('#cards-graphs-amount')
-    var $todayTotal = $cardAmount.find('#dayAmount')
-    var $storeInfo = appTab.find('#home-cards')
-    var $storeName = $storeInfo.find('#home-name')
-    var $storeLayout = $storeInfo.find('#home-layout')
-    var $storeID = $storeInfo.find('#home-id')
-    var storeId = localStorage.getItem('store_id')
-    var $lastOrders = appTab.find('#lastOrders')
-    var $order = $lastOrders.find('#addOrders')
-    var $approve = appTab.find('#approved')
-    var $monthTotal = appTab.find('#monthAmount')
-    var $lastCardAmount = appTab.find('#cards-graphs-amount-last')
-    var $lastApprove = appTab.find('#lastApproved')
-    var $lastMonthTotal = appTab.find('#lastMonthAmount')
-    var $lastTotal = $lastCardAmount.find('#lastDayAmount')
-    var $monthTotalOrders = appTab.find('#monthTotalOrders')
-    var $monthTotalOrdersApr = appTab.find('#monthTotalOrdersApproved')
-    var $lastMonthTotalOrders = appTab.find('#lastMonthTotalOrders')
-    var $lastMonthTotalOrdersApr = appTab.find('#lastMonthTotalOrdersApproved')
-    var calcWithTimezone = function (timeZone) {
+  const setup = function () {
+    const appTab = $('#app-tab-' + tabId)
+    let countPending = 0
+    let countPaid = 0
+    let countCancelled = 0
+    const $cardAmount = appTab.find('#cards-graphs-amount')
+    const $todayTotal = $cardAmount.find('#dayAmount')
+    const $storeInfo = appTab.find('#home-cards')
+    const $storeName = $storeInfo.find('#home-name')
+    const $storeLayout = $storeInfo.find('#home-layout')
+    const $storeID = $storeInfo.find('#home-id')
+    const storeId = localStorage.getItem('store_id')
+    const $lastOrders = appTab.find('#lastOrders')
+    const $order = $lastOrders.find('#addOrders')
+    const $approve = appTab.find('#approved')
+    const $monthTotal = appTab.find('#monthAmount')
+    const $lastCardAmount = appTab.find('#cards-graphs-amount-last')
+    const $lastApprove = appTab.find('#lastApproved')
+    const $lastMonthTotal = appTab.find('#lastMonthAmount')
+    const $lastTotal = $lastCardAmount.find('#lastDayAmount')
+    const $monthTotalOrders = appTab.find('#monthTotalOrders')
+    const $monthTotalOrdersApr = appTab.find('#monthTotalOrdersApproved')
+    const $lastMonthTotalOrders = appTab.find('#lastMonthTotalOrders')
+    const $lastMonthTotalOrdersApr = appTab.find('#lastMonthTotalOrdersApproved')
+    const calcWithTimezone = (timeZone) => {
       if (timeZone < 0) {
         return -1 * timeZone
       } else {
         return timeZone
       }
     }
-    var pad = function (number) {
+    const pad = (number) => {
       if (number < 10) {
         return '0' + number
       }
       return number
     }
-    var getISOWithLocalStart = function (date) {
+    const getISOWithLocalStart = (date) => {
       return date.getFullYear() +
         '-' + pad(date.getMonth() + 1) +
         '-' + pad(date.getDate()) +
@@ -54,7 +54,7 @@ export default function () {
         ':' + pad(date.getSeconds()) +
         '.000Z'
     }
-    var getISOWithLocalEnd = function (date) {
+    const getISOWithLocalEnd = (date) => {
       return date.getFullYear() +
         '-' + pad(date.getMonth() + 1) +
         '-' + pad(date.getDate()) +
@@ -63,28 +63,28 @@ export default function () {
         ':' + pad(date.getSeconds()) +
         '.999Z'
     }
-    var today = new Date()
-    var dd = today.getDate()
-    var mm = today.getMonth()
-    var yyyy = today.getFullYear()
-    var timezoneCalc = calcWithTimezone(new Date().getTimezoneOffset())
-    var dataStartYesterday = getISOWithLocalStart(new Date(yyyy, mm, dd - 1, 0, timezoneCalc, 0, 0))
-    var dataEndYesterday = getISOWithLocalEnd(new Date(yyyy, mm, dd - 1, 23, 59 + timezoneCalc, 59, 0))
-    var dateStartTwo = getISOWithLocalStart(new Date(yyyy, mm - 1, 1, 0, timezoneCalc, 0, 0))
+    const today = new Date()
+    const dd = today.getDate()
+    const mm = today.getMonth()
+    const yyyy = today.getFullYear()
+    const timezoneCalc = calcWithTimezone(new Date().getTimezoneOffset())
+    const dataStartYesterday = getISOWithLocalStart(new Date(yyyy, mm, dd - 1, 0, timezoneCalc, 0, 0))
+    const dataEndYesterday = getISOWithLocalEnd(new Date(yyyy, mm, dd - 1, 23, 59 + timezoneCalc, 59, 0))
+    const dateStartTwo = getISOWithLocalStart(new Date(yyyy, mm - 1, 1, 0, timezoneCalc, 0, 0))
     // var dateEndTwo = getISOWithLocalEnd(new Date(yyyy, mm - 1, 31, 23, 59 + timezoneCalc, 59, 0))
-    var dataStart = getISOWithLocalStart(new Date(yyyy, mm, dd, 0, timezoneCalc, 0, 0))
-    var dataEnd = getISOWithLocalEnd(new Date(yyyy, mm, dd, 23, 59 + timezoneCalc, 59, 0))
-    var dateStart = getISOWithLocalStart(new Date(yyyy, mm, 1, 0, timezoneCalc, 0, 0))
-    var dateEnd = getISOWithLocalEnd(new Date(yyyy, mm, 31, 23, 59 + timezoneCalc, 59, 0))
+    const dataStart = getISOWithLocalStart(new Date(yyyy, mm, dd, 0, timezoneCalc, 0, 0))
+    const dataEnd = getISOWithLocalEnd(new Date(yyyy, mm, dd, 23, 59 + timezoneCalc, 59, 0))
+    const dateStart = getISOWithLocalStart(new Date(yyyy, mm, 1, 0, timezoneCalc, 0, 0))
+    const dateEnd = getISOWithLocalEnd(new Date(yyyy, mm, 31, 23, 59 + timezoneCalc, 59, 0))
 
-    var approved = 0
-    var approvedLast = 0
-    var urlOrderLastMonth = 'orders.json?sort=amount&created_at>=' + dateStartTwo + '&created_at<=' + dateStart + '&fields=amount,created_at,financial_status'
-    var urlOrderMonth = 'orders.json?sort=amount&created_at>=' + dateStart + '&fields=buyers,amount,_id,created_at,financial_status,number'
+    let approved = 0
+    let approvedLast = 0
+    const urlOrderLastMonth = 'orders.json?sort=amount&created_at>=' + dateStartTwo + '&created_at<=' + dateStart + '&fields=amount,created_at,financial_status'
+    const urlOrderMonth = 'orders.json?sort=amount&created_at>=' + dateStart + '&fields=buyers,amount,_id,created_at,financial_status,number'
     $storeID.text(storeId)
-    var urlStore = 'stores/me.json'
+    const urlStore = 'stores/me.json'
     // search for store name and object id
-    var sumOfAmount = function (a, b) {
+    const sumOfAmount = (a, b) => {
       return {
         amount: {
           total: a.amount.total + b.amount.total
@@ -93,7 +93,7 @@ export default function () {
     }
     window.callApi(urlStore, 'GET', function (error, schema) {
       if (!error) {
-        var storeName = schema.name
+        const storeName = schema.name
         if (schema.homepage) {
           $storeLayout.append(
             '  <a target="_blank" data-lang="en_us" href="' + schema.homepage + '/admin" class="form-text">Access layout store</a>' +
@@ -118,9 +118,9 @@ export default function () {
       setTimeout(function () {
         window.callApi(urlOrderMonth, 'GET', function (err, json) {
           if (!err) {
-            var result = json.result
+            const result = json.result
 
-            var filteredToday = result.filter(function (item) {
+            const filteredToday = result.filter(function (item) {
               return item.created_at >= dataStart && item.created_at <= dataEnd
             })
 
@@ -133,24 +133,24 @@ export default function () {
             })
 
             if (filteredMonth.length) {
-              var filteredMonthPaid = filteredMonth.filter(function (paid) {
-                if (paid.financial_status) {
+              const filteredMonthPaid = filteredMonth.filter(function (paid) {
+                if (paid.financial_status && paid.financial_status.current === 'paid') {
                   return paid.financial_status.current === 'paid'
                 }
               })
               if (filteredMonthPaid.length) {
-                var monthAmount = filteredMonthPaid.reduce(sumOfAmount)
+                const monthAmount = filteredMonthPaid.reduce(sumOfAmount)
                 $monthTotal.text(window.ecomUtils.formatMoney(monthAmount.amount.total, 'BRL'))
-                var order = filteredMonth.length
+                const order = filteredMonth.length
                 approved = filteredMonthPaid.length
-                var percentApproved = ((approved / order) * 100).toFixed(2)
+                const percentApproved = ((approved / order) * 100).toFixed(2)
                 $approve.text(percentApproved)
                 $monthTotalOrders.text(order)
                 $monthTotalOrdersApr.text(approved)
               }
             }
             if (filteredLast.length) {
-              var amountLast = filteredLast.reduce(sumOfAmount)
+              const amountLast = filteredLast.reduce(sumOfAmount)
               $lastTotal.text(window.ecomUtils.formatMoney(amountLast.amount.total, 'BRL'))
             }
 
@@ -158,9 +158,9 @@ export default function () {
               appTab.find('#cards-graphs-orders').show()
               const todayAmount = filteredToday.reduce(sumOfAmount)
               $todayTotal.text(window.ecomUtils.formatMoney(todayAmount.amount.total, 'BRL'))
-              for (var i = 0; i < filteredToday.length; i++) {
+              for (let i = 0; i < filteredToday.length; i++) {
                 if (filteredToday[i].financial_status) {
-                  var orderInfo = []
+                  const orderInfo = []
                   orderInfo.push(filteredToday[i]._id, filteredToday[i].number, filteredToday[i].financial_status.current, filteredToday[i].amount.total.toFixed(2).replace('.', ','), filteredToday[i].buyers[0]._id, filteredToday[i].buyers[0].display_name)
                   switch (orderInfo[2]) {
                     case 'under_analysis':
@@ -203,7 +203,7 @@ export default function () {
                   '  <td> R$ ' + orderInfo[3] + '</td>' +
                   '  <td><a href="/#/resources/customers/' + orderInfo[4] + ' ">' + orderInfo[5] + ' </a></td>' +
                   '</tr>')
-                  var statusOrder = filteredToday[i].financial_status.current
+                  const statusOrder = filteredToday[i].financial_status.current
                   if (statusOrder === 'paid') {
                     countPaid = countPaid + 1
                   }
@@ -225,7 +225,7 @@ export default function () {
           if (!err) {
             const lastMonthData = data.result
             if (lastMonthData.length) {
-              var filteredLastMonthPaid = lastMonthData.filter(function (paid) {
+              const filteredLastMonthPaid = lastMonthData.filter(function (paid) {
                 if (paid.financial_status) {
                   return paid.financial_status.current === 'paid'
                 }
@@ -233,9 +233,9 @@ export default function () {
               if (filteredLastMonthPaid) {
                 const lastMonthAmount = filteredLastMonthPaid.reduce(sumOfAmount)
                 $lastMonthTotal.text(window.ecomUtils.formatMoney(lastMonthAmount.amount.total, 'BRL'))
-                var orders = lastMonthData.length
+                const orders = lastMonthData.length
                 approvedLast = filteredLastMonthPaid.length
-                var percentApprovedLast = ((approvedLast / orders) * 100).toFixed(2)
+                const percentApprovedLast = ((approvedLast / orders) * 100).toFixed(2)
                 $lastApprove.text(percentApprovedLast)
                 $lastMonthTotalOrders.text(orders)
                 $lastMonthTotalOrdersApr.text(approvedLast)
@@ -245,9 +245,9 @@ export default function () {
         })
       }, 100)
     })
-    var bar = function () {
-      var ctx = document.getElementById('orderOfAll')
-      var myChart = new Chart(ctx, {
+    const bar = function () {
+      const ctx = document.getElementById('orderOfAll')
+      const myChart = new Chart(ctx, {
         type: 'bar',
         data: {
           labels: ['Aprovados', 'Pendentes', 'Cancelados'],
