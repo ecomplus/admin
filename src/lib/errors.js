844d3384f9a447fb6b7d51b958b668a1cf51fafe
Leonardo Matos <leomp120894@gmail.com>
2021-02-19 22:58:07 -0300

    chore(lib): importing from @ecomplus/admin-helpers

--- src/lib/errors.js
+++ src/lib/errors.js
@@ -1,5 +1,5 @@
-import { $ecomConfig, i18n } from '@ecomplus/utils'
-import toast from './toast'
+import { i18n } from '@ecomplus/utils'
+import { handleApiError } from '@ecomplus/admin-helpers'
 
 const { localStorage, sessionStorage, $ } = window
 
@@ -17,39 +17,6 @@ const handleFatalError = err => {
     $(window).off('beforeunload')
     window.location.reload()
   }, 3000)
-}
-
-const handleApiError = data => {
-  let msg
-  if (typeof data === 'object' && data !== null) {
-    if (data.user_message) {
-      msg = data.user_message[$ecomConfig.get('lang')].replace(/^data\.?/i, '')
-      if (msg.charAt(0) === ' ') {
-        msg = msg.charAt(1).toUpperCase() + msg.substr(2)
-      }
-      const enMsg = data.user_message.en_us
-      let field = enMsg.replace(/^.*'([\w.]+)'.*$/, '$1')
-      if (field.indexOf(' ') === -1) {
-        if (enMsg.startsWith('data.')) {
-          field = enMsg.replace(/^data.([\w.]+)\s.*$/, '$1') + '.' + field
-        }
-        $(`input[name="${field}"]:visible`).focus()
-      }
-    } else if (data.message) {
-      msg = data.message
-    }
-  }
-  if (msg !== undefined) {
-    console.log(`API Error Code: ${data.error_code}`)
-  } else {
-    msg = i18n({
-      en_us: 'Unknown error, please try again',
-      pt_br: 'Erro desconhecido, por favor tente novamente'
-    })
-  }
-  toast(msg, {
-    duration: 7000
-  })
 }
 
 export { handleFatalError, handleApiError }
