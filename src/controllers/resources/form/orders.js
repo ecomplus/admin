7d1444dfe46143eeff60577b4746b531fa6f5f63
Matheus Reis <matheusgnreis@gmail.com>
2022-08-09 15:50:40 -0300

    chore(form/orders): change some information about subscriptions

--- src/controllers/resources/form/orders.js
+++ src/controllers/resources/form/orders.js
@@ -17,6 +17,8 @@ export default function () {
     handleNestedObjects
   } = window
 
+  const { i19OrderStatus, i19subscriptions } = require('@ecomplus/i18n')
+
   // current tab ID
   var Tab = window.Tabs[tabId]
   // edit JSON document
@@ -85,59 +87,12 @@ export default function () {
     }
     */
 
-    // render subscriptions
-    const parseStatus = status => {
-      if (status === 'open') return 'Em aberto'
-      else if (status === 'closed') return 'Finalizado'
-      else return 'Cancelado'
-    }
-    const $listOfSubscriptions = elContainer.find('#t' + tabId + '-order-subscription')
-    const renderSubs = orders => {
-      if (orders.length) {
-        return orders.reduce((trsStr, order, i) => trsStr + `
-              <tr>
-                <td>${(i + 1)}</td>
-                <td><a href="/resources/orders/${order._id}">${order.number}</a></td>
-                ${(order.status ? `<td>${parseStatus(order.status)}</td>` : '')}
-                <td>${formatMoney(order.amount.total)}</td>
-                <td>${formatDate(order.created_at)}</td>
-              </tr>`, '')
-      }
-    }
-    if (orderId) {
-      let url
-      if (Data().transactions[0].type === 'recurrence') {
-        url = `orders.json?subscription_order._id=${orderId}`
-        callApi(url, 'GET', (err, json) => {
-          if (!err) {
-            const orders = json.result
-            console.log(orders)
-            $listOfSubscriptions.append(renderSubs(orders))
-          }
-        })
-      } else if (Data().subscription_order && Data().subscription_order._id) {
-        const order = Data().subscription_order.number
-        url = `orders.json?number=${order}`
-        callApi(url, 'GET', (err, json) => {
-          if (!err) {
-            console.log(json)
-            const order = json.result
-            $listOfSubscriptions.append(renderSubs(order))
-          }
-        })
-      } else {
-        document.getElementById('subscriptions').style.display = 'none'
-      }
-    }
-
     // render buyers blocks
-    var $buyers, $buyerInfo
+    const $buyers = $orderBase.find('#t' + tabId + '-order-buyers')
+    const $buyerInfo = $buyers.find('#t' + tabId + '-buyer-info')
     // BUG: sometimes $buyers and $buyerInfo was empty
     // FIX: delay to fix bug
     setTimeout(function () {
-      $buyers = $orderBase.find('#t' + tabId + '-order-buyers')
-      $buyerInfo = $buyers.find('#t' + tabId + '-buyer-info')
-
       // show current buyers from list if any
       if (data.buyers) {
         for (var i = 0; i < data.buyers.length; i++) {
@@ -345,6 +300,41 @@ export default function () {
       callApi(uri, 'GET', callback, null, skipError)
     }
 
+    // render subscriptions
+    const $listOfSubscriptions = elContainer.find('#t' + tabId + '-invoices')
+    if (orderId) {
+      let url
+      const data = Data()
+      if (data.transactions[0].type === 'recurrence') {
+        document.getElementById('subscription-invoices').style.display = 'block'
+        url = `orders.json?subscription_order._id=${orderId}&fields=_id,number,status,amount,created_at`
+        callApi(url, 'GET', (err, json) => {
+          if (!err) {
+            const orders = json.result
+            $listOfSubscriptions.append(
+              orders.reduce((trsStr, order, i) => trsStr + `
+              <tr>
+                <td>${(i + 1)}</td>
+                <td><a href="/resources/orders/${order._id}">${order.number}</a></td>
+                ${(order.status ? `<td>${i18n(i19OrderStatus)[order.status]}</td>` : '')}
+                <td>${formatMoney(order.amount.total)}</td>
+                <td>${formatDate(order.created_at)}</td>
+              </tr>`, '')
+            )
+          }
+        })
+      } else if (data.subscription_order && data.subscription_order._id) {
+        url = `orders.json?_id=${data.subscription_order._id}&limit=1&fields=_id,number`
+        callApi(url, 'GET', (err, json) => {
+          if (!err) {
+            const order = json.result[0]
+            $buyers.after(`
+            <h4 class="mt-30"><a href="/resources/orders/${order._id}">${i18n(i19subscriptions)} #${order.number}</a></h4>
+            `)
+          }
+        })
+      }
+    }
     // reuse order status enum and respective colors from lists configuration JSON
     import(/* webpackChunkName: "data_misc_config-lists" */ '@/data/misc/config-lists')
       .then(exp => {
