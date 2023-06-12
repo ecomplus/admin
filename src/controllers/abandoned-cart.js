5b8eb8927e302f0e7fcf34caefb563831c68640f
matheusgnreis <matheus@e-com.club>
2023-06-12 20:29:13 -0300

    chore(abandoned-cart): get 200 customer per request instead all of them

--- src/controllers/abandoned-cart.js
+++ src/controllers/abandoned-cart.js
@@ -38,40 +38,104 @@ export default function () {
 
   const datatable = $('#abandoned-cart').DataTable(datatableOptions)
 
-  const renderListByDate = (start, end, customers) => {
+  const renderTable = (customers, result, datatable) => {
+    const rows = []
+    if (Array.isArray(result) && result.length) {
+      result.forEach(cart => {
+        const selectedCustomer = customers.find(customer => customer._id === cart._id)
+        if (selectedCustomer) {
+          let name = ''
+          if (selectedCustomer.name) {
+            if (selectedCustomer.name.given_name) {
+              name += selectedCustomer.name.given_name
+            }
+            if (selectedCustomer.name.family_name) {
+              name += ' ' + selectedCustomer.name.family_name
+            } 
+          }
+          rows.push([
+            name,
+            selectedCustomer.email,
+            selectedCustomer.phone && selectedCustomer.phone.length && selectedCustomer.phone[0] || 'Sem número',
+            urlCart ? urlCart + cart.cartId : 'Sem url configurada',
+            formatMoney(cart.subtotal)
+          ])
+        }
+      })
+      datatable.clear()
+      datatable.rows.add(rows)
+      datatable.draw()
+      $('#abandoned-cart-table-load').hide()
+    }
+  }
+
+  const renderListByDate = async (start, end) => {
+    datatable.clear()
+    datatable.draw()
+    $('#abandoned-cart-table-load').show()
     callApi(
       '$aggregate.json',
       'POST',
-      (err, json) => {
+      async (err, json) => {
         if (!err) {
           const { result } = json
-          const rows = []
           if (Array.isArray(result) && result.length) {
+            const carts = []
+            const ids = []
+            let customers = []
             result.forEach(cart => {
-              const selectedCustomer = customers.find(customer => customer._id === cart._id)
-              if (selectedCustomer) {
-                let name = ''
-                if (selectedCustomer.name) {
-                  if (selectedCustomer.name.given_name) {
-                    name += selectedCustomer.name.given_name
-                  }
-                  if (selectedCustomer.name.family_name) {
-                    name += ' ' + selectedCustomer.name.family_name
-                  } 
-                }
-                rows.push([
-                  name,
-                  selectedCustomer.email,
-                  selectedCustomer.phone && selectedCustomer.phone.length && selectedCustomer.phone[0] || 'Sem número',
-                  urlCart ? urlCart + cart.cartId : 'Sem url configurada',
-                  formatMoney(cart.subtotal)
-                ])
-              }
+              carts.push(cart)
+              ids.push(cart._id)
             })
-            datatable.clear()
-            datatable.rows.add(rows)
-            datatable.draw()
-            $('#abandoned-cart-table-load').hide()
+            const sizeArray = 200
+            let arraySliced
+            let init = 0
+            const loops = ids.length > 200 ? Math.ceil(ids.length / sizeArray) : 1
+            const rows = []
+            for (let index = 0; index < loops; index++) {
+              init++
+              arraySliced = ids.slice(index * sizeArray, sizeArray * init)
+              callApi(
+                '$aggregate.json',
+                'POST',
+                (err, json) => {
+                  if (!err) {
+                    customers = customers.concat(json.result)
+                    if ((index + 1) === loops) {
+                      renderTable(customers, carts, datatable)
+                    }
+                  }
+                },
+                {
+                  resource: 'customers',
+                  pipeline: [
+                    {
+                      $match: {
+                        'login': true,
+                        '_id': {
+                          $in: arraySliced
+                        }
+            
+                      }
+                    },
+                    {
+                      $group: {
+                        _id: '$_id',
+                        email: {
+                          $first: '$main_email'
+                        },
+                        name: {
+                          $first: '$name'
+                        },
+                        phone: {
+                          $first: '$phones.number'
+                        }
+                      }
+                    }
+                  ]
+                }
+              )
+            }
           }
         }
       },
@@ -84,11 +148,7 @@ export default function () {
                 $gte: start.toISOString(),
                 $lte: end.toISOString(),
               },
-              'completed': {
-                $in: [
-                  false
-                ]
-              }
+              'completed': false
             }
           },
           {
@@ -133,83 +193,45 @@ export default function () {
   $('#datepicker-abandoned [data-when="start"]').datepicker('setDate', new Intl.DateTimeFormat('pt-br').format(start));
   $('#datepicker-abandoned [data-when="end"]').datepicker('setDate', new Intl.DateTimeFormat('pt-br').format(end));
 
-  callApi(
-    '$aggregate.json',
-    'POST',
-    (err, json) => {
-      if (!err) {
-        const { result } = json
-        if (Array.isArray(result) && result.length) {
-          renderListByDate(start, end, result)
+  renderListByDate(start, end)
 
-          $('#datepicker-abandoned').datepicker({}).on('changeDate', (e) => {
-            if (e.date) {
-              if (e.target && e.target.dataset && e.target.dataset.when) {
-                type = e.target.dataset.when
-                if (type === 'start') {
-                  start = normalizeDate(0, 0, 0, 0, false, e.date)
-                } else if (type === 'end') {
-                  end = normalizeDate(23, 59, 59, 59, false, e.date)
-                }
-                if (start && end) {
-                  renderListByDate(start, end, result)
-                }
-              }
-            }
-          })
-          const $exportBestSeller = $('#export-abandoned-cart')
-          const downloadCsv = exportData => {
-            const columns = [i18n(i19name), i18n(i19email), i18n(i19number), i18n(i19permalink), i18n(i19total)]
-            const csv = Papa.unparse({
-              data: exportData,
-              fields: columns
-            })
-            const csvData = new window.Blob([csv], {
-              type: 'text/csv;charset=utf-8;'
-            })
-            const csvURL = navigator.msSaveBlob
-              ? navigator.msSaveBlob(csvData, 'download.csv')
-              : window.URL.createObjectURL(csvData)
-            const $link = document.createElement('a')
-            $link.href = csvURL
-            $link.setAttribute('download', 'export-abandoned-cart.csv')
-            $link.click()
-            $(`#t${tabId}-loading`).hide()
-          }
-          $exportBestSeller.click(() => {
-            downloadCsv(datatable.rows().data())
-          })
+  $('#datepicker-abandoned').datepicker({}).on('changeDate', (e) => {
+    if (e.date) {
+      if (e.target && e.target.dataset && e.target.dataset.when) {
+        type = e.target.dataset.when
+        if (type === 'start') {
+          start = normalizeDate(0, 0, 0, 0, false, e.date)
+        } else if (type === 'end') {
+          end = normalizeDate(23, 59, 59, 59, false, e.date)
+        }
+        if (start && end) {
+          renderListByDate(start, end)
         }
       }
-    },
-    {
-      resource: 'customers',
-      pipeline: [
-        {
-          $match: {
-            'login': {
-              $in: [true]
-            },
-            'enabled': {
-              $in: [true]
-            }
-          }
-        },
-        {
-          $group: {
-            _id: '$_id',
-            email: {
-              $first: '$main_email'
-            },
-            name: {
-              $first: '$name'
-            },
-            phone: {
-              $first: '$phones.number'
-            }
-          }
-        }
-      ]
     }
-  )
+  })
+
+  const $exportBestSeller = $('#export-abandoned-cart')
+  const downloadCsv = exportData => {
+  const columns = [i18n(i19name), i18n(i19email), i18n(i19number), i18n(i19permalink), i18n(i19total)]
+  const csv = Papa.unparse({
+    data: exportData,
+    fields: columns
+  })
+  const csvData = new window.Blob([csv], {
+    type: 'text/csv;charset=utf-8;'
+  })
+  const csvURL = navigator.msSaveBlob
+    ? navigator.msSaveBlob(csvData, 'download.csv')
+    : window.URL.createObjectURL(csvData)
+  const $link = document.createElement('a')
+  $link.href = csvURL
+  $link.setAttribute('download', 'export-abandoned-cart.csv')
+  $link.click()
+  $(`#t${tabId}-loading`).hide()
+  }
+  $exportBestSeller.click(() => {
+    downloadCsv(datatable.rows().data())
+  })
 }
+
